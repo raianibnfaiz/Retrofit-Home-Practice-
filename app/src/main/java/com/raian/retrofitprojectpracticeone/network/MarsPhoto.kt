97320ba0a2a89ea0a6b9f7b370480cd730bee1b5
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2023-01-12 23:38:56 +0600

    added an api and displayed the numbers of the photos of that api

---
+++ app/src/main/java/com/raian/retrofitprojectpracticeone/network/MarsPhoto.kt
@@ -0,0 +1,6 @@
+package com.raian.retrofitprojectpracticeone.network
+
+data class MarsPhoto(
+    val id: String,
+    val img_src: String
+)
---
+++ app/src/main/java/com/raian/retrofitprojectpracticeone/overview/OverviewViewModel.kt
@@ -0,0 +1,39 @@
+package com.raian.retrofitprojectpracticeone.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import com.raian.retrofitprojectpracticeone.network.MarsApi
+
+class OverviewViewModel : ViewModel(){
+
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<String> = _status
+    /**
+     * Call getMarsPhotos() on init so we can display status immediately.
+     */
+    init {
+        getMarsPhotos()
+    }
+
+    /**
+     * Gets Mars photos information from the Mars API Retrofit service and updates the
+     * [MarsPhoto] [List] [LiveData].
+     */
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
+        }
+    }
+}
